9363820de1678ce41a06524c4a7f3ad8d6ae5b4f
Inhoob <inhoob@gmail.com>
2022-06-27 00:17:26 +0900

    Font 변경기능 추가

--- virtual keyboard/src/js/keyboard.js
+++ virtual keyboard/src/js/keyboard.js
@@ -1,5 +1,6 @@
 export class Keyboard {
   #switchEl; //private field 사용 Keyboard 클래스를 생성하면 switchEl이 발생한다
+  #fontSelectEl;
   constructor() {
     this.#assignElement();
     this.#addEvent();
@@ -7,6 +8,7 @@ export class Keyboard {
 
   #assignElement() {
     this.#switchEl = document.getElementById("switch");
+    this.#fontSelectEl = document.getElementById("font");
   }
   #addEvent() {
     this.#switchEl.addEventListener("change", (event) => {
@@ -14,6 +16,9 @@ export class Keyboard {
         "theme",
         event.target.checked ? "dark-mode" : ""
       );
+    });
+    this.#fontSelectEl.addEventListener("change", (event) => {
+      document.body.style.fontFamily = event.target.value;
     });
   }
 }
