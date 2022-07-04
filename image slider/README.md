ad74f30caedb5f8113a72c05ae8345650befcbbb
Inhoob <inhoob@gmail.com>
2022-07-04 21:13:09 +0900

    인디케이터 개발

--- image slider/README.md
+++ image slider/README.md
@@ -9,3 +9,7 @@
   height: 400px;
 }
 ```
+
+Indicator를 눌렀을 때 모든 Indicator에 addEvent를 해 줄 수는 없기 때문에 Event Bubbling을 이용해 indicatorWrap에서 클릭하면 해당 인덱스로 이동하도록 했다.
+
+Autoplay 기능은 control-wrap play/pause 로 아이콘이 각각 뜨게 해뒀다.
--- image slider/src/js/imageSlider.js
+++ image slider/src/js/imageSlider.js
@@ -17,6 +17,8 @@ export default class imageSlider {
     this.initSlideWidth();
     this.initSliderListWidth();
     this.addEvent();
+    this.createIndicator();
+    this.setIndicator();
   }
 
   assignElement() {
@@ -24,6 +26,7 @@ export default class imageSlider {
     this.sliderListEl = this.sliderWrapEl.querySelector('#slider');
     this.nextBtnEl = this.sliderWrapEl.querySelector('#next');
     this.previousBtnEl = this.sliderWrapEl.querySelector('#previous');
+    this.indicatorWrapEl = this.sliderWrapEl.querySelector('#indicator-wrap');
   }
 
   initSlideNumber() {
@@ -41,6 +44,21 @@ export default class imageSlider {
   addEvent() {
     this.nextBtnEl.addEventListener('click', this.moveToRight.bind(this));
     this.previousBtnEl.addEventListener('click', this.moveToLeft.bind(this));
+    this.indicatorWrapEl.addEventListener(
+      'click',
+      this.onClickIndicator.bind(this),
+    );
+  }
+
+  onClickIndicator(event) {
+    const indexPosition = parseInt(event.target.dataset.index, 10); // ES lint때문에 10진법으로 넣어준다. event가 string으로 발생하기 때문에 이렇게 해줬음
+    if (Number.isInteger(indexPosition)) {
+      this.#currentPosition = indexPosition;
+      this.sliderListEl.style.left = `-${
+        this.#slideWidth * this.#currentPosition
+      }px`;
+    }
+    this.setIndicator(); // position이 바꼈으니까
   }
 
   moveToRight() {
@@ -51,6 +69,7 @@ export default class imageSlider {
     this.sliderListEl.style.left = `-${
       this.#slideWidth * this.#currentPosition
     }px`;
+    this.setIndicator();
   }
 
   moveToLeft() {
@@ -61,5 +80,28 @@ export default class imageSlider {
     this.sliderListEl.style.left = `-${
       this.#slideWidth * this.#currentPosition
     }px`;
+    this.setIndicator();
+  }
+
+  /* 그 사이에 아무것도 안들어가기 때문에 docFragment를 썼다. 이 부분 다시 공부하면서 정리할 것 */
+  createIndicator() {
+    const docFragment = document.createDocumentFragment();
+    for (let i = 0; i < this.#slideNumber; i += 1) {
+      const li = document.createElement('li');
+      li.dataset.index = i;
+      docFragment.appendChild(li);
+    }
+    this.indicatorWrapEl.querySelector('ul').appendChild(docFragment);
+  }
+
+  // 활성화된 index가 없었다가 index에 따라서 활성화
+
+  setIndicator() {
+    this.indicatorWrapEl.querySelector('li.active')?.classList.remove('active'); // 처음에 active 가 없을 수 있으니까 optional chaining
+    this.indicatorWrapEl
+      .querySelector(
+        `ul li:nth-child(${this.#currentPosition + 1})`, // nth child는 1부터 시작하는거라 +1
+      )
+      .classList.add('active');
   }
 }
