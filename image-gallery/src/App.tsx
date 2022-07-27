ca53d00e4c2b9b2151b149e15b04ca68a455477b
Inhoob <inhoob@gmail.com>
2022-07-27 21:38:55 +0900

    imagebox를 map으로 출력, key를 unique하게 정하는방법

--- image-gallery/src/App.tsx
+++ image-gallery/src/App.tsx
@@ -1,24 +1,30 @@
 import React from "react";
 import ImageBox from "./components/ImageBox";
 import "./App.css";
-import { useRef } from "react";
+import { useRef, useState } from "react";
 
 function App() {
   const inpRef = useRef<HTMLInputElement>(null);
-
+  const [imageList, setImageList] = useState<string[]>([]); //string list라는 뜻
   return (
     <div className="container">
       <div className="initial-box">
-        <div className="text-center">
-          이미지가 없습니다
-          <br />
-          이미지를 추가해주세요.
-        </div>
+        {imageList.length === 0 && ( //imageBox길이가 0일때 이미지가 없다고 출력하는 방식. 이런 방식으로 코드를 줄일 수 있다.
+          <div className="text-center">
+            이미지가 없습니다
+            <br />
+            이미지를 추가해주세요.
+          </div>
+        )}
+
         <input
           type="file"
           ref={inpRef}
           onChange={(event) => {
-            console.log(event.currentTarget.value);
+            const v = event.currentTarget.value;
+            console.log(imageList);
+            const reader = new FileReader();
+            setImageList((prev) => [...prev, v]);
           }}
         />
         <div
@@ -29,6 +35,9 @@ function App() {
         >
           +
         </div>
+        {imageList.map((el, idx) => (
+          <ImageBox key={el + idx} src={el} />
+        ))}
       </div>
     </div>
   );
