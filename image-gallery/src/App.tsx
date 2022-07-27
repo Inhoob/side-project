42622055596870e287a56ea51e600b8dfd759230
Inhoob <inhoob@gmail.com>
2022-07-27 21:24:22 +0900

    파일선택 버튼을 + 버튼이랑 매핑

--- image-gallery/src/App.tsx
+++ image-gallery/src/App.tsx
@@ -1,8 +1,11 @@
 import React from "react";
 import ImageBox from "./components/ImageBox";
 import "./App.css";
+import { useRef } from "react";
 
 function App() {
+  const inpRef = useRef<HTMLInputElement>(null);
+
   return (
     <div className="container">
       <div className="initial-box">
@@ -11,8 +14,15 @@ function App() {
           <br />
           이미지를 추가해주세요.
         </div>
-        <input type="file" />
-        <div className="plus-box mt">+</div>
+        <input type="file" ref={inpRef} />
+        <div
+          className="plus-box"
+          onClick={() => {
+            inpRef.current?.click();
+          }}
+        >
+          +
+        </div>
       </div>
     </div>
   );
