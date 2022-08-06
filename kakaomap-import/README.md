16caaea487b81f4fcf607163f43b380d248611da
Inhoob <inhoob@gmail.com>
2022-08-06 15:56:22 +0900

    kakaomap import

--- kakaomap-import/README.md
+++ kakaomap-import/README.md
@@ -3,3 +3,5 @@
 map을 불러오는 script를 public 폴더의 index.html 에 넣어준다.
 
 맨 처음에 바로 error 가 나오는 이유? web 플랫폼에서 내가 사용할 url을 지정해주면 제대로 동작
+
+외부에서 kakaomap api 같은 것을 가져올 때 반드시 declare로 window 내의 kakao를 정의해줘야 쓸 수 있고, window 내의 객체를 불러오는 방식으로 사용한다.
--- kakaomap-import/src/App.tsx
+++ kakaomap-import/src/App.tsx
@@ -1,21 +1,37 @@
-import React, { useEffect } from "react";
+import React, { useEffect, useRef } from "react";
 
 import "./App.css";
 
+//kakao를 바로 사용할 수 없다. typescript에서는 카카오가 있다고 알려줘야함.
+declare global {
+  interface Window {
+    kakao: any;
+  }
+}
 function App() {
+  const mapRef = useRef<HTMLDivElement>(null);
   useEffect(() => {
-    var container = document.getElementById("map"); //지도를 담을 영역의 DOM 레퍼런스
-    var options = {
-      //지도를 생성할 때 필요한 기본 옵션
-      center: new kakao.maps.LatLng(33.450701, 126.570667), //지도의 중심좌표.
-      level: 3, //지도의 레벨(확대, 축소 정도)
-    };
+    const script = document.createElement("script");
+    script.src =
+      "//dapi.kakao.com/v2/maps/sdk.js?appkey=8ddefe020a79e0665ee5cb987238f5c7&autoload=false";
+    document.head.appendChild(script);
 
-    var map = new kakao.maps.Map(container, options); //지도 생성 및 객체 리턴
+    script.onload = () => {
+      window.kakao.maps.load(() => {
+        if (mapRef.current) {
+          var options = {
+            center: new window.kakao.maps.LatLng(33.450701, 126.570667), //지도의 중심좌표.
+            level: 3, //지도의 레벨(확대, 축소 정도)
+          };
+          var map = new window.kakao.maps.Map(mapRef.current, options); //지도 생성 및 객체 리턴
+        }
+      });
+    };
+    return () => script.remove();
   }, []);
   return (
     <>
-      <div id="map"></div>
+      <div ref={mapRef} style={{ width: 300, height: 300 }}></div>
     </>
   );
 }
